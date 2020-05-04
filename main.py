332819e84c2dd795274747c19c75612a6397ed71
habds <matiasmallealira@gmail.com>
2020-05-04 02:17:42 -0400

    bd falsa3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from bdfalsa import productos
 app = Flask(__name__)
 
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return 'hola'
+    return jsonify(productos)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
