f14747b63d315bd464b3744bcd85c58bbd168c6a
habds <matiasmallealira@gmail.com>
2020-05-04 00:53:59 -0400

    bd falsa2

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
+from bdfalsa import productos
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
 
+@app.route('/')
+def inicio():
+    return 'hola'
